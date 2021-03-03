c845916f1458e54bdb15b6ddcf24b9fd7b0cd2c1
Mahantesh R <41316852+MahanteshR@users.noreply.github.com>
2021-03-03 12:55:19 +0530

    initial commit

---
+++ helloWorld/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// How to run - go run main.go
+// Package main - {package == project == workspace}
+// * Execultable - Generates a executable file that can be run
+// * Reusable - code used as "helpers", resusable code.
+// 
