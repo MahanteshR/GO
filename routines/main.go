d0640b35ff8d91715daa295f03c498491b4d4b2d
Mahantesh R <41316852+MahanteshR@users.noreply.github.com>
2021-03-19 11:17:40 +0530

    routines

---
+++ routines/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"https://www.google.com",
+		"https://www.amazon.com",
+		"https://www.apple.com",
+		"https://www.flipkart.com",
+		"https://www.cisco.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	fmt.Println(<-c)
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "is down")
+		c <- "Might be down"
+		return
+	}
+	fmt.Println(link, "is running ")
+	c <- "the link is running"
+}
